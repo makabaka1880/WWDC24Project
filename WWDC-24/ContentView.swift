6895992265ad914951426345dd0f62a49d1f1c13
makabaka1880 <makabaka1880@outlook.com>
2023-12-16 19:52:31 +0800

    Initial Commit

---
+++ WWDC-24/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WWDC-24
+//
+//  Created by SeanLi on 12/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WWDC-24/WWDC_24App.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC_24App.swift
+//  WWDC-24
+//
+//  Created by SeanLi on 12/16/23.
+//
+
+import SwiftUI
+
+@main
+struct WWDC_24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
